f4d3dfba615ecbbed8a6506050eb32ae4e707fd8
Min <solunar@vip.qq.com>
2024-01-02 22:34:30 +0800

    Add tailwindcss

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-import './assets/main.css';
-
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 
 import App from './App.vue';
 import router from './router';
+
+import './css/app.scss';
 
 const app = createApp(App);
 
---
+++ src/views/home.vue
@@ -0,0 +1,7 @@
+<script lang="ts" setup>
+defineOptions({ name: 'app-home' });
+</script>
+
+<template>
+  <div class="h-screen flex justify-center items-center">Lorem ipsum dolor sit amet.</div>
+</template>
