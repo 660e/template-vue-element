e8af57c69eede82941cccb0f7ab8c96f287f1b64
Min <solunar@vip.qq.com>
2024-01-02 22:58:21 +0800

    Update

--- src/views/home.vue
+++ src/views/home.vue
@@ -7,8 +7,9 @@ const input = ref();
 </script>
 
 <template>
-  <div class="h-screen flex justify-center items-center">
-    <div class="space-x-2">
+  <div class="h-screen flex flex-col justify-center items-center">
+    <img src="@/assets/logo.svg" class="w-24" />
+    <div class="space-x-2 mt-8">
       <el-input v-model="input" class="w-48" placeholder="Please input" />
       <el-button type="primary">Primary</el-button>
     </div>
