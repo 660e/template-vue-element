0b176a63d74626315f1a6c8f3a625606e35cf4b1
Min <solunar@vip.qq.com>
2024-01-02 22:53:18 +0800

    Add element-plus

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { createPinia } from 'pinia';
 
 import App from './App.vue';
 import router from './router';
+import ElementPlus from 'element-plus';
 
 import './css/app.scss';
 
@@ -10,5 +11,6 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(ElementPlus);
 
 app.mount('#app');
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,7 +1,16 @@
 <script lang="ts" setup>
+import { ref } from 'vue';
+
 defineOptions({ name: 'app-home' });
+
+const input = ref();
 </script>
 
 <template>
-  <div class="h-screen flex justify-center items-center">Lorem ipsum dolor sit amet.</div>
+  <div class="h-screen flex justify-center items-center">
+    <div class="space-x-2">
+      <el-input v-model="input" class="w-48" placeholder="Please input" />
+      <el-button type="primary">Primary</el-button>
+    </div>
+  </div>
 </template>
